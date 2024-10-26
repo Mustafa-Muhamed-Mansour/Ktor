a9e0c5ab28e5d7ecbe13e967c957283730b64bc7
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-10-26 17:43:23 +0300

    explain play "function" (without || with) install the plugin "Routing"

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,32 @@
-@file:Suppress("UNUSED_EXPRESSION")
-
 package com.example
 
 import com.example.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.routing.*
 
 fun main() {
-    "بسم الله نبدأ مشروع كامل بالمتطلبات اللازمة لفهم ktor بطريقة مُبسطة"
+    embeddedServer(factory = Netty, port = 8888, host = "127.0.0.1") {
+    /* without install */
+    // withoutInstall()
+
+    /* with install */
+    // install(plugin = Routing) {
+    // withInstall()
+    // }
+
+    }.start(wait = true)
 }
 
+  // here i don't install the plugin "Routing",  why?
+    // because i define extension func. as an "Application" so it's not important to install
+/* fun Application.withoutInstall() {
+    configureWithoutInstallRouting()
+} */
+
+  // here i did install the plugin "Routing",  why?
+    // because i define extension func. as an "Routing" so it's important to install
+/* fun Routing.withInstall() {
+      configureWithInstallRouting()
+} */
---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,19 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+/* fun Application.configureWithoutInstallRouting() {
+    routing {
+        get("/") {
+            call.respond("Hello World, without install")
+        }
+    }
+} */
+
+/* fun Routing.configureWithInstallRouting() {
+        get("/") {
+            call.respond("Hello World, with install")
+        }
+} */
