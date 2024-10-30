36f9e11f8eff87c7d1659388e7965dd77acf2a0e
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-10-30 03:43:16 +0300

    here i receive "data || request" by data class "<DataClass>" and i selected type a status. ^ don't forget add impl. serialization, content, plugin serialization, install content, add json in the body content, and add annotation serializable in the DataClass ^

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,20 @@
 package com.example
 
-import com.example.plugins.sendDataOrRequestByBody
+import com.example.plugins.receiveDataOrRequest
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 fun main() {
     embeddedServer(factory = Netty, port = 8888, host = "127.0.0.1") {
-
+        // here install content, ok why?
+           // because without وجود "a json || content" can't transformation تحويل from data class to json
+        install(plugin = ContentNegotiation) {
+            json()
+        }
+        receiveDataOrRequestFromPostman()
        // sendDataOrRequestByBodyFromPostman()
 
     }.start(wait = true)
@@ -20,3 +27,10 @@ fun main() {
 /* fun Application.sendDataOrRequestByBodyFromPostman() {
        sendDataOrRequestByBody()
 } */
+
+   // here I receive "data || request" by data class
+      // (don't forget choose Body then raw then choose JSON instead of Text)
+fun Application.receiveDataOrRequestFromPostman() {
+    receiveDataOrRequest()
+}
+
