e634c3e4308aa2cc8f70ff7de62ee8ebec553f34
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-11-06 01:57:15 +0200

    here i reading all values from sql by using ktorm ( i meaning kotlin )

---
+++ src/main/kotlin/com/example/plugin/readAllValues.kt
@@ -0,0 +1,25 @@
+package com.example.plugin
+
+import com.example.entity.ProductModel
+import org.ktorm.database.Database
+import org.ktorm.dsl.from
+import org.ktorm.dsl.select
+
+fun readAllValues() {
+    /* here I connection ktor with database " SQL " after installing program " MySQL Workbench "  */
+      // url = localhost.port " 127.0.0.1:3306 " ---> in the first page in program MySQL when a open the project
+      // driver = this library follow تبع the driver can to connect mySQL
+      // url = userName " root " ---> in the first page in program MySQL when a open the project
+      // password = password for me, i'm speeched كتبته when i install program MySQL Workbench
+    val database = Database.connect(
+        url = "jdbc:mysql://127.0.0.1:3306/products",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "password for you"
+    )
+
+    val readAllValue = database.from(table = ProductModel).select()
+    for (raws in readAllValue) {
+        println(message = "ID = ${raws[ProductModel.id]} ....... Product = ${raws[ProductModel.product]}")
+    }
+}
