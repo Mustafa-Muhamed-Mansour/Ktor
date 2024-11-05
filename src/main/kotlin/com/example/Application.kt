59a940eae9f3664b432789577fce6ff5bb1ec6b1
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-11-05 23:12:27 +0200

    updated code inside body of embeddedServer in "fileApplication"

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -6,20 +6,21 @@ import org.ktorm.database.Database
 
 fun main() {
     embeddedServer(factory = Netty, port = 8888, host = "127.0.0.1") {
-    }
 
+    /* here I connection ktor with database " SQL " after installing program " MySQL Workbench "  */
+        // url = localhost.port " 127.0.0.1:3306 " ---> in the first page in program MySQL when a open the project
+        // driver = this library follow تبع the driver can to connect mySQL
+        // url = userName " root " ---> in the first page in program MySQL when a open the project
+        // password = password for me, i'm speeched كتبته when i install program MySQL Workbench
 
- /* here I connection ktor with database " SQL " after installing program " MySQL Workbench "  */
-    // url = localhost.port " 127.0.0.1:3306 " ---> in the first page in program MySQL when a open the project
-    // driver = this library follow تبع the driver can to connect mySQL
-    // url = userName " root " ---> in the first page in program MySQL when a open the project
-    // password = password for me, i'm speeched كتبته when i install program MySQL Workbench
-    val database = Database.connect(
-        url = "jdbc:mysql://127.0.0.1:3306/products",
-        driver = "com.mysql.cj.jdbc.Driver",
-        user = "root",
-        password = "password for you, you are speeched when you install program ^MySQL Workbench^ "
-    )
+        val database = Database.connect(
+            url = "jdbc:mysql://127.0.0.1:3306/products",
+            driver = "com.mysql.cj.jdbc.Driver",
+            user = "root",
+            password = "MostAfA01556044553%"
+        )
+    }.start(wait = true)
+
 }
 
 
