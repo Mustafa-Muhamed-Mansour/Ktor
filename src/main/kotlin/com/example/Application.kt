70bddcf66eb3f65cae099d04e2bbd6aa06ad001e
Mostafa Mohamed <mostafamansour163@yahoo.com>
2024-11-06 02:09:48 +0200

    here i enhancement from delete value selected by using ktorm (i meaning يعني kotlin)

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,40 +1,23 @@
 package com.example
 
-import com.example.entity.ProductModel
+import com.example.plugin.deleteValueSelected
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.ktorm.database.Database
-import org.ktorm.dsl.delete
-import org.ktorm.dsl.eq
-import org.ktorm.dsl.insert
 
 fun main() {
     embeddedServer(factory = Netty, port = 8888, host = "127.0.0.1") {
-
-    /* here I connection ktor with database " SQL " after installing program " MySQL Workbench "  */
-      // url = localhost.port " 127.0.0.1:3306 " ---> in the first page in program MySQL when a open the project
-      // driver = this library follow تبع the driver can to connect mySQL
-      // url = userName " root " ---> in the first page in program MySQL when a open the project
-      // password = password for me, i'm speeched كتبته when i install program MySQL Workbench
-        val database = Database.connect(
-            url = "jdbc:mysql://127.0.0.1:3306/products",
-            driver = "com.mysql.cj.jdbc.Driver",
-            user = "root",
-            password = "password for you"
-        )
-
-
-      // here i delete value selected by using ktorm (i meaning يعني kotlin) by object الموجود in package entity and he "ProductModel"
+        // here i delete value selected by using ktorm (i meaning يعني kotlin) by object الموجود in package entity and he "ProductModel"
             // and everyThing showing in sql
-        database.delete(table = ProductModel) {
-            it.id eq 2
-        }
-
+        deleteValueSelectedByUsingKtorm()
 
     }.start(wait = true)
 
 }
 
+
+fun deleteValueSelectedByUsingKtorm() {
+    deleteValueSelected()
+}
 
 
 
---
+++ src/main/kotlin/com/example/plugin/deleteValueSelected.kt
@@ -0,0 +1,25 @@
+package com.example.plugin
+
+import com.example.entity.ProductModel
+import org.ktorm.database.Database
+import org.ktorm.dsl.delete
+import org.ktorm.dsl.eq
+
+fun deleteValueSelected() {
+
+    /* here I connection ktor with database " SQL " after installing program " MySQL Workbench "  */
+      // url = localhost.port " 127.0.0.1:3306 " ---> in the first page in program MySQL when a open the project
+      // driver = this library follow تبع the driver can to connect mySQL
+      // url = userName " root " ---> in the first page in program MySQL when a open the project
+      // password = password for me, i'm speeched كتبته when i install program MySQL Workbench
+    val database = Database.connect(
+        url = "jdbc:mysql://127.0.0.1:3306/products",
+        driver = "com.mysql.cj.jdbc.Driver",
+        user = "root",
+        password = "password for you"
+    )
+
+    database.delete(table = ProductModel) {
+        it.id eq 3
+    }
+}
